96691905a0d578344467a4395e7f96f4eaabc694
josegutierrezmorals <josegutierrezmorals@gmail.com>
2026-05-10 18:45:30 -0400

    Update server.js
    Signed-off-by: josegutierrezmorals <josegutierrezmorals@gmail.com>

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const cors       = require('cors');
 const helmet     = require('helmet');
-
 const apiRoutes        = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner           = require('./test-runner');
@@ -16,15 +15,15 @@ app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  helmet.contentSecurityPolicy({
+app.use(helmet({
+  contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'"],
       styleSrc: ["'self'"],
     },
-  })
-);
+  },
+}));
 
 app.route('/').get(function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
